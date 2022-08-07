e37325f1796ba413a6c719cf30fb1d224d3064c6
harmatiuk <lucasjharmatiuk@gmail.com>
2022-08-07 00:27:11 -0300

    add gitignore

--- inicio.py
+++ inicio.py
@@ -1,6 +1,7 @@
 from tabnanny import check
 from parsel import Selector
-from functions import generate_list_with_urls_catalogue, read_page_and_return_html, infos_current_page
+import re
+from functions import create_file, generate_list_with_urls_catalogue, read_page_and_return_html, infos_current_page
 
 url_base = 'http://books.toscrape.com'
 
@@ -13,4 +14,11 @@ infos_main_page = infos_current_page(response)
 main_page = infos_main_page['current_pag']
 max_page = infos_main_page['max_pag']
 
-print(generate_list_with_urls_catalogue(url_base, max_page))
+catalogue_page_list = generate_list_with_urls_catalogue(url_base, max_page)
+
+
+for item in catalogue_page_list:
+    soup = read_page_and_return_html(item)
+    pag = re.findall("[0-9]+", item)[0]
+    name = f"page-{pag}"
+    create_file(name, "html", soup)
